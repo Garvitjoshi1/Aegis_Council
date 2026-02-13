77050109beddc44dd93b0cdf4f7f01d8ce682d5d
Garvit Joshi <garvitjoshi46@gmail.com>
2026-02-13 22:12:54 +0530

    learning

---
+++ crash_course/server-setup/server.py
@@ -0,0 +1,22 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP(
+    name = "Calculator",
+    host = "0.0.0.0", # for SSE transport(localhost)
+    port = 8050
+)
+
+@mcp.tool()
+def add(a:int, b:int) -> int:
+    return a+b
+
+if __name__ == "__main__":
+    transport = "sse"
+    if transport == "stdio":
+        print("Running server with stdio transport")
+        mcp.run(transport="stdio")
+    elif transport =="sse":
+        print("Running server with SSE transport")
+        mcp.run(transport="sse")
+    else:
+        raise ValueError(f"Unknown transport: {transport}")
